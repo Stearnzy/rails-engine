607487dacf1d875dbea5fa1ea6f8eb612b586c15
Zach Stearns <zach.stearns92@gmail.com>
2020-12-13 21:20:29 -0700

    Write show test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -17,4 +17,20 @@ describe 'Merchant' do
       expect(merchant[:name]).to be_a(String)
     end
   end
+
+  it 'returns a merchant by id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
 end
