1fa16d7e5abec93619f02a171a55a1b9da7d6491
Zach Stearns <zach.stearns92@gmail.com>
2020-12-13 20:29:36 -0700

    First part of merchant request test

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -7,5 +7,14 @@ describe 'Merchant' do
     get '/api/v1/merchants'
 
     expect(response).to be_sucessful
+
+    # merchants = JSON.parse(response.body, symbolize_names: true)
+
+    # expect(merchants.count).to eq(5)
+
+    # merchants.each do |merchant|
+    #   expect(merchant).to have_key(:name)
+    #   expect(merchant[:name]).to be_a(String)
+    # end
   end
 end
