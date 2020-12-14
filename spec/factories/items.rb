c75d1d0430cbc8b0290f92b3f46eb2a7473fbe28
Zach Stearns <zach.stearns92@gmail.com>
2020-12-14 14:41:15 -0700

    Customize item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :item do
-    name { "" }
-    description { "" }
-    unit_price { "" }
+    name { Faker::Commerce.product_name }
+    description { Faker::Commerce.material }
+    unit_price { Faker::Commerce.price }
     merchant { nil }
   end
 end
