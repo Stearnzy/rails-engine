ec396893be13e5cfe40920f8f399c9ca478202e1
Zach Stearns <zach.stearns92@gmail.com>
2020-12-14 14:09:04 -0700

    Add index test for items

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Item' do
+  it 'returns index of items' do
+    create_list(:item, 10, merchant: create(:merchant))
+
+    get "/api/v1/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items[:data].count).to eq(10)
+
+    items[:data].each do |item|
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+    end
+  end
+end
