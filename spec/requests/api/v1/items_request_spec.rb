d92ade768de64a1260f0056239738f3acbeef01f
Zach Stearns <zach.stearns92@gmail.com>
2020-12-14 14:41:29 -0700

    Write show test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -23,4 +23,24 @@ describe 'Item' do
       expect(item[:attributes][:unit_price]).to be_a(Float)
     end
   end
+
+  it 'finds an item by id' do
+    create(:item, name: 'Sunglasses', merchant: create(:merchant))
+    glasses = Item.last
+
+    get "/api/v1/items/#{glasses.id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a String
+
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a String
+
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a Float
+  end
 end
