affe9d4ef3552a67bdfcaaf26625b0f008230f85
Zach Stearns <zach.stearns92@gmail.com>
2020-12-13 21:52:37 -0700

    Create action and strong params

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: Merchant.find(params[:id])
   end
+
+  def create
+    render json: Merchant.create(merchant_params)
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
