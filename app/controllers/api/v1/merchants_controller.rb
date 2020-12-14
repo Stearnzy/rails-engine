85ba87964e1d973d3091272b96fad6a75d5639cc
Zach Stearns <zach.stearns92@gmail.com>
2020-12-14 16:16:28 -0700

    Add MerchantSerializer to other crud actions

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,20 +8,20 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def create
-    render json: Merchant.create(merchant_params)
+    render json: MerchantSerializer.new(Merchant.create(merchant_params))
   end
 
   def update
-    render json: Merchant.update(params[:id], merchant_params)
+    render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
   end
 
   def destroy
-    render json: Merchant.delete(params[:id])
+    Merchant.delete(params[:id])
   end
 
   private
 
   def merchant_params
-    params.require(:merchant).permit(:name)
+    params.permit(:name)
   end
 end
