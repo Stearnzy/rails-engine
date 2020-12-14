40547d8569d6d09b0fdd93923ea8d4b3e3313e91
Zach Stearns <zach.stearns92@gmail.com>
2020-12-14 13:44:04 -0700

    Add merchant serializer to index and show

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: Merchant.all
+    render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show
-    render json: Merchant.find(params[:id])
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 
   def create
