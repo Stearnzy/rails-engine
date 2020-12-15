3ed6a1e22b52ac1e87e70b55a7ad175820d32adc
Zach Stearns <zach.stearns92@gmail.com>
2020-12-14 17:27:26 -0700

    Create test written

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -4,7 +4,7 @@ describe 'Item' do
   it 'returns index of items' do
     create_list(:item, 10, merchant: create(:merchant))
 
-    get "/api/v1/items"
+    get '/api/v1/items'
 
     expect(response).to be_successful
 
@@ -15,7 +15,7 @@ describe 'Item' do
     items[:data].each do |item|
       expect(item[:attributes]).to have_key(:name)
       expect(item[:attributes][:name]).to be_a(String)
-      
+
       expect(item[:attributes]).to have_key(:description)
       expect(item[:attributes][:description]).to be_a(String)
 
@@ -33,7 +33,7 @@ describe 'Item' do
     expect(response).to be_successful
 
     item = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(item[:data][:attributes]).to have_key(:name)
     expect(item[:data][:attributes][:name]).to be_a String
 
@@ -42,5 +42,23 @@ describe 'Item' do
 
     expect(item[:data][:attributes]).to have_key(:unit_price)
     expect(item[:data][:attributes][:unit_price]).to be_a Float
+  end
+
+  it 'creates an item' do
+    merch_id = create(:merchant).id
+
+    item_params = { name: 'Top Hat', description: 'Goes on your head.',
+                    unit_price: 42.50, merchant_id: merch_id }
+    headers = { 'CONTENT-TYPE' => 'application/json' }
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
   end
 end
