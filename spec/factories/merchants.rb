fd9577a78f04bf479038ef492301a50292c391f5
Zach Stearns <zach.stearns92@gmail.com>
2020-12-14 17:57:13 -0700

    Add with_items trait

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :merchant do
     name { Faker::Company.name }
+  
+    trait :with_items do
+      after :create do |merchant|
+        create_list(:item, 4, merchant: merchant)
+      end
+    end
   end
 end
