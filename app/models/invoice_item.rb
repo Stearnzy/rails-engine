8f2ca6d0ab322e086a798771f83ecad9de467404
Zach Stearns <zach.stearns92@gmail.com>
2020-12-14 21:31:42 -0700

    Create model validations and relationships

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,2 +1,6 @@
 class InvoiceItem < ApplicationRecord
+  validates_presence_of :quantity, :unit_price
+  
+  belongs_to :invoice
+  belongs_to :item
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,8 @@
 class Merchant < ApplicationRecord
+  validates_presence_of :name
+
   has_many :items, dependent: :destroy
+  has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 end
