50a28486250367ab699d3ef8104bfeebaaeff1ef
Zach Stearns <zach.stearns92@gmail.com>
2020-12-16 12:28:31 -0700

    Add route for item multi search

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       end
       namespace :items do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       resources :merchants do
         get '/items', to: 'merchants/items#index'
