b20f5674d45d5918913caf27816e24f0f34cda4f
Zach Stearns <zach.stearns92@gmail.com>
2020-12-15 21:58:04 -0700

    Add find_one show path

---
+++ app/controllers/api/v1/items/search_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::Items::SearchController < ApplicationController
+  def show
+    render json: ItemSerializer.new(Item.find_one(search_key, search_value))
+  end
+
+  private
+
+  def search_params
+    params.permit(:name, :description, :unit_price, :created_at, :updated_at)
+  end
+
+  def search_key
+    search_params.keys.first
+  end
+
+  def search_value
+    search_params[search_key]
+  end
+end
