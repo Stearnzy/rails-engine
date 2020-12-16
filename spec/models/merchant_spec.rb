9845e904c7ffc6bb929891b265a17079d47470b8
Zach Stearns <zach.stearns92@gmail.com>
2020-12-15 19:58:41 -0700

    Add tests for find_one method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,4 +11,30 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoice_items).through(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
   end
+
+  describe 'model methods' do
+    before(:each) do
+      @merchant_1 = Merchant.create!(name: 'The Rusty Bucket', created_at: '2020-12-13', updated_at: '2020-12-15')
+      @merchant_2 = Merchant.create!(name: 'The Clean Spoon', created_at: '2020-11-11', updated_at: '2020-12-01')
+    end
+
+    describe '#find_one' do
+      it 'finds based on name' do
+        expect(Merchant.find_one('name', 'The Rusty Bucket')).to eq(@merchant_1)
+      end
+
+      it 'finds based on partial search' do
+        expect(Merchant.find_one('name', 'Rus')).to eq(@merchant_1)
+      end
+
+      it 'finds case insensitive' do
+        expect(Merchant.find_one('name', 'ClEAn')).to eq(@merchant_2)
+      end
+
+      it 'finds one' do
+        result = [Merchant.find_one('name', 'The')]
+        expect(result.count).to eq(1)
+      end
+    end
+  end
 end
