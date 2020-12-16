0b1e58f1544e2c8a6e90f67bc2d1aeafee2baef1
Zach Stearns <zach.stearns92@gmail.com>
2020-12-15 19:40:32 -0700

    Add find_one method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,8 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.find_one(search_key, search_value)
+    Merchant.find_by("LOWER(#{search_key}) LIKE ?", "%#{search_value.downcase}%")
+  end
 end
