38c5afc1feb94d4d940ffddfee0c2e3a3ddfb458
Zach Stearns <zach.stearns92@gmail.com>
2020-12-15 22:03:11 -0700

    Add find_one method for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.find_one(search_key, search_value)
+    Item.find_by("LOWER(#{search_key}) LIKE ?", "%#{search_value.downcase}%")
+  end
 end
