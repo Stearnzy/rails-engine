69478499714fc334d6a0e1350368300ef380c845
Zach Stearns <zach.stearns92@gmail.com>
2020-12-15 19:39:32 -0700

    Add find one route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#show'
+      end
       resources :merchants do
         get '/items', to: 'merchants/items#index'
       end
-      
+
       resources :items do
         get '/merchants', to: 'items/merchants#show'
       end
