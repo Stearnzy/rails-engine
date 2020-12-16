282dbfcd1e692d999e3e2901ab7e3a9debb55a6a
Zach Stearns <zach.stearns92@gmail.com>
2020-12-16 15:07:45 -0700

    Add date search conditionals

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,16 +9,16 @@ class Merchant < ApplicationRecord
   def self.find_one(search_key, search_value)
     if search_key == 'name'
       Merchant.find_by("LOWER(#{search_key}) LIKE ?", "%#{search_value.downcase}%")
-    # elsif search_key == 'created_at' || search_key == 'updated_at'
-
+    elsif search_key == 'created_at' || search_key == 'updated_at'
+      Merchant.find_by("#{search_key} = '%#{search_value.to_date}%'")
     end
   end
 
   def self.find_all(search_key, search_value)
     if search_key == 'name'
       Merchant.where("LOWER(#{search_key}) LIKE ?", "%#{search_value.downcase}%")
-    # elsif search_key == 'created_at' || search_key == 'updated_at'
-
+    elsif search_key == 'created_at' || search_key == 'updated_at'
+      Merchant.where("#{search_key} = '%#{search_value.to_date}%'")
     end
   end
 end
