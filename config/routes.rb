a9ebad1ad093da16f65d9ea760fb044b0b1cef43
Zach Stearns <zach.stearns92@gmail.com>
2020-12-16 18:27:44 -0700

    Make route for most_revenue

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
+        get '/most_revenue', to: 'revenue#index'
       end
       namespace :items do
         get '/find', to: 'search#show'
