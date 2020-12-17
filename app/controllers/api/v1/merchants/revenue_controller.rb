ea9bcbcf5d84d67a68f6c0d53dc65cbb703b666b
Zach Stearns <zach.stearns92@gmail.com>
2020-12-16 21:06:23 -0700

    Add show action for merchant revenue

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::Merchants::RevenueController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity].to_i))
   end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.total_revenue(params[:id].to_i))
+  end
 end
