1aac4e86c735c18a3f915ab8e6a309808f1bb52e
Zach Stearns <zach.stearns92@gmail.com>
2020-12-16 20:29:03 -0700

    Add most items sold route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/most_revenue', to: 'revenue#index'
+        get '/most_items', to: 'most_items_sold#index'
       end
       namespace :items do
         get '/find', to: 'search#show'
