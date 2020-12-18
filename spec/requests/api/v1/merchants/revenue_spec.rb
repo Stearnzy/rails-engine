792e16a4219bc6c074b191b0bf8a5f018d933d46
Zach Stearns <zach.stearns92@gmail.com>
2020-12-17 17:13:05 -0700

    Finish test for revenue between dates

--- spec/requests/api/v1/merchants/revenue_spec.rb
+++ spec/requests/api/v1/merchants/revenue_spec.rb
@@ -9,39 +9,39 @@ describe 'Most Revenue' do
     @merch5 = create(:merchant)
 
     @inv1 = create(:invoice, merchant_id: @merch1.id, updated_at: '2020-02-01')
-    create(:transaction, result: "success", invoice_id: @inv1.id)
+    create(:transaction, result: 'success', invoice_id: @inv1.id)
     create(:invoice_item, quantity: 1, unit_price: 20.00, invoice_id: @inv1.id, item_id: create(:item, unit_price: 20.00).id)
 
     @inv2 = create(:invoice, merchant_id: @merch2.id, updated_at: '2020-03-01')
-    create(:transaction, result: "success", invoice_id: @inv2.id)
+    create(:transaction, result: 'success', invoice_id: @inv2.id)
     create(:invoice_item, quantity: 2, unit_price: 20.00, invoice_id: @inv2.id, item_id: create(:item, unit_price: 20.00).id)
 
     @inv3 = create(:invoice, merchant_id: @merch3.id, updated_at: '2020-04-01')
-    create(:transaction, result: "success", invoice_id: @inv3.id)
+    create(:transaction, result: 'success', invoice_id: @inv3.id)
     create(:invoice_item, quantity: 3, unit_price: 20.00, invoice_id: @inv3.id, item_id: create(:item, unit_price: 20.00).id)
 
     @inv4 = create(:invoice, merchant_id: @merch4.id, updated_at: '2020-05-01')
-    create(:transaction, result: "success", invoice_id: @inv4.id)
+    create(:transaction, result: 'success', invoice_id: @inv4.id)
     create(:invoice_item, quantity: 4, unit_price: 20.00, invoice_id: @inv4.id, item_id: create(:item, unit_price: 20.00).id)
 
     @inv5 = create(:invoice, merchant_id: @merch5.id, updated_at: '2020-06-01')
-    create(:transaction, result: "success", invoice_id: @inv5.id)
+    create(:transaction, result: 'success', invoice_id: @inv5.id)
     create(:invoice_item, quantity: 5, unit_price: 20.00, invoice_id: @inv5.id, item_id: create(:item, unit_price: 20.00).id)
 
     @inv6 = create(:invoice, merchant_id: @merch1.id, updated_at: '2020-07-01')
-    create(:transaction, result: "failed", invoice_id: @inv6.id)
+    create(:transaction, result: 'failed', invoice_id: @inv6.id)
     create(:invoice_item, quantity: 10, unit_price: 20.00, invoice_id: @inv6.id, item_id: create(:item, unit_price: 20.00).id)
 
-    @inv7 = create(:invoice, merchant_id: @merch2.id, status: "packaged", updated_at: '2020-08-01')
-    create(:transaction, result: "success", invoice_id: @inv7.id)
+    @inv7 = create(:invoice, merchant_id: @merch2.id, status: 'packaged', updated_at: '2020-08-01')
+    create(:transaction, result: 'success', invoice_id: @inv7.id)
     create(:invoice_item, quantity: 15, unit_price: 20.00, invoice_id: @inv7.id, item_id: create(:item, unit_price: 20.00).id)
 
     @inv8 = create(:invoice, merchant_id: @merch3.id, updated_at: '2020-09-01')
-    create(:transaction, result: "success", invoice_id: @inv8.id)
+    create(:transaction, result: 'success', invoice_id: @inv8.id)
     create(:invoice_item, quantity: 20, unit_price: 20.00, invoice_id: @inv8.id, item_id: create(:item, unit_price: 20.00).id)
 
     @inv9 = create(:invoice, merchant_id: @merch2.id, updated_at: '2020-10-01')
-    create(:transaction, result: "success", invoice_id: @inv9.id)
+    create(:transaction, result: 'success', invoice_id: @inv9.id)
     create(:invoice_item, quantity: 32, unit_price: 20.00, invoice_id: @inv9.id, item_id: create(:item, unit_price: 20.00).id)
   end
 
@@ -63,7 +63,7 @@ describe 'Most Revenue' do
     merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
       expect(merchant).to have_key(:type)
-      expect(merchant[:type]).to eq("merchant")
+      expect(merchant[:type]).to eq('merchant')
       expect(merchant).to have_key(:attributes)
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
@@ -102,7 +102,14 @@ describe 'Most Revenue' do
 
     revenue_data = JSON.parse(response.body, symbolize_names: true)
 
-    
-    require 'pry'; binding.pry
+    expect(revenue_data).to have_key(:data)
+    expect(revenue_data[:data]).to have_key(:id)
+    expect(revenue_data[:data][:id]).to be_nil
+
+    expect(revenue_data[:data]).to have_key(:attributes)
+    expect(revenue_data[:data][:attributes]).to be_a Hash
+    expect(revenue_data[:data][:attributes]).to have_key(:revenue)
+    expect(revenue_data[:data][:attributes][:revenue]).to be_a Float
+    expect(revenue_data[:data][:attributes][:revenue]).to eq(expected_revenue)
   end
 end
