e212066074c82a5b403aa52749efd3c067a9f105
Zach Stearns <zach.stearns92@gmail.com>
2020-12-17 19:23:19 -0700

    Add checks for empty and reversed dates, and error messages

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::RevenueController < ApplicationController
   def show
-    if DateTime.parse(params[:start]) <= DateTime.parse(params[:end])
-      render json: RevenueSerializer.revenue(RevenueFacade.total_revenue_over_time(params[:start], params[:end]))
-    else
+    if params[:start].empty? || params[:end].empty?
+      empty_date_error
+    elsif DateTime.parse(params[:start]) > DateTime.parse(params[:end])
       backwards_date_error
+    elsif DateTime.parse(params[:start]) <= DateTime.parse(params[:end])
+      render json: RevenueSerializer.revenue(RevenueFacade.total_revenue_over_time(params[:start], params[:end]))
     end
   end
 
@@ -13,6 +15,14 @@ class Api::V1::RevenueController < ApplicationController
     render json: JSON.generate(
       {
         error: 'end date occurs before start date'
+      }
+    )
+  end
+
+  def empty_date_error
+    render json: JSON.generate(
+      {
+        error: 'dates cannot be empty'
       }
     )
   end
