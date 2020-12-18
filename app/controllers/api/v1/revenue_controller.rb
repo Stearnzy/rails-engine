cedc6f5945e6a36fb498d4a0ad8a1845e09d6c1b
Zach Stearns <zach.stearns92@gmail.com>
2020-12-17 20:09:42 -0700

    Add datetime parsable helper and introduce error checks for invalid date entries

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::RevenueController < ApplicationController
   def show
-    if params[:start].empty? || params[:end].empty?
-      empty_date_error
-    elsif DateTime.parse(params[:start]) > DateTime.parse(params[:end])
-      backwards_date_error
-    elsif DateTime.parse(params[:start]) <= DateTime.parse(params[:end])
-      render json: RevenueSerializer.revenue(RevenueFacade.total_revenue_over_time(params[:start], params[:end]))
+    if DateTime.parsable?(params[:start]) && DateTime.parsable?(params[:end])
+      if DateTime.parse(params[:start]) > DateTime.parse(params[:end])
+        backwards_date_error
+      elsif DateTime.parse(params[:start]) <= DateTime.parse(params[:end])
+        render json: RevenueSerializer.revenue(RevenueFacade.total_revenue_over_time(params[:start], params[:end]))
+      end
+    else
+      date_invalid_error
     end
   end
+
+
 
   private
 
@@ -19,11 +23,20 @@ class Api::V1::RevenueController < ApplicationController
     )
   end
 
-  def empty_date_error
+  def date_invalid_error
     render json: JSON.generate(
       {
-        error: 'dates cannot be empty'
+        error: 'dates must be valid'
       }
     )
+  end
+
+  def DateTime.parsable?(string)
+    begin
+      parse(string)
+      true
+    rescue ArgumentError
+      false
+    end
   end
 end
